db627770ba22259a9e0b6c83922f786fe7dbe228
Harsh <sharshvardhan759@gmail.com>
2024-09-01 19:13:37 +0530

    Learned why we need express,js.

--- node-practice/heyy.js
+++ node-practice/heyy.js
@@ -4,7 +4,17 @@ const server = http.createServer((req , res) => {
     
     if(req.url == '/'){
        const data =  fs.readFileSync("./Public/home.html")
-       res.end(data)
+        //we are using readFileSync because we are reading the file synchronously
+        //if we use readFile then it will read the file asynchronously
+        //we need to use readFile when we are reading a large file
+       console.log(req.url)
+       console.log(req.method)
+       console.log(req.headers)
+       res.writeHead(200 , {"Content-Type" : "text/html"}) // to tell the browser that the content is of type html i.e. header
+       // content type matters, if I change it to text/plain then the html code will be displayed.
+       res.write(data) // to write the response to the client
+       res.end() // to send the response to the client, if we don't send the response then the client will keep loading
+       //res.end is different from res.write because res.end sends the response to the client and stops the server from running
     }else if(req.url == '/about'){
         const d =  fs.readFileSync("./Public/about.html")
         res.end(d)
@@ -15,6 +25,10 @@ const server = http.createServer((req , res) => {
         res.end("<h1>404 Page Not Found! :(</h1>");
     }
 
+    // the main problem with this code is that we have to write the code for each and every route
+    // to solve this problem we can use express.js
+    // for instance if we have many resourcess like styles.css, about.html, contact.html, home.html etc then we have to write the code for each and every resource
+
     
 })
 const PORT = process.env.PORT || 3002
@@ -22,4 +36,5 @@ const HOST = 'localhost'
 server.listen(PORT , HOST , () => {
     console.log("server runs at port no. - http://localhost:3002")
 });
+
 
