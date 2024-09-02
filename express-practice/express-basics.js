8949f87ed7996cd83f3485fb881ac18bec5f82f4
Harsh <sharshvardhan759@gmail.com>
2024-09-02 11:27:43 +0530

    learned express basics

---
+++ express-practice/express-basics.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+app.get('/' , (req , res) => {
+    res.send("<h1>Home Page</h1>")
+})
+
+app.get('/about' , (req , res) => {
+    res.status(200).send("<h1>About Page</h1>") // it's a good practice to send status code
+})
+
+app.get('/contact' , (req , res) => {
+    res.send("<h1>Contact Page</h1>")
+})
+
+app.all('*' , (req , res) => {
+    res.status(404).send("<h1>404 Page Not Found! :(</h1>")
+})
+
+
+app.listen(3000 , () => {
+    console.log("Server runs at port no. - http://localhost:3000")
+})
